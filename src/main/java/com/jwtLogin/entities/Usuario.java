263efea02f896fd4230ca9d217057002119e4b7a
João Victor Duarte <joaodicu387@gmail.com>
2022-04-25 20:57:30 -0300

    Refatorando entidade Usuario

--- src/main/java/com/jwtLogin/entities/Usuario.java
+++ src/main/java/com/jwtLogin/entities/Usuario.java
@@ -15,39 +15,21 @@ public class Usuario {
 	private Long id;
 	private String email;
 	private String password;
-	private String role = "USER";
 	
 	public Usuario() {
-		role = "USER";
+		
 	}
 	
 	public Usuario(String email, String password) {
 		this.email = email;
 		this.password = password;
-		role = "USER";
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	public String getEmail() {
 		return email;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
 	public String getPassword() {
 		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 }
