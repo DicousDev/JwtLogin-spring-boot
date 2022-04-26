7f601ce805a2dec35ab9729344e751912dec850f
João Victor Duarte <joaodicu387@gmail.com>
2022-04-25 21:01:40 -0300

    Refatorando JwtRequest

--- src/main/java/com/jwtLogin/dto/JwtRequest.java
+++ src/main/java/com/jwtLogin/dto/JwtRequest.java
@@ -5,10 +5,6 @@ public class JwtRequest {
 	private String email;
 	private String password;
 	
-	public JwtRequest() {
-		
-	}
-	
 	public JwtRequest(String email, String password) {
 		this.email = email;
 		this.password = password;
@@ -18,15 +14,7 @@ public class JwtRequest {
 		return email;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
 	public String getPassword() {
 		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
 	}
 }
